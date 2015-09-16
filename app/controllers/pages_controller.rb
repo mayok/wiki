211dd6216432b32984d4e2fab382a528b2ca9b05
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2015-09-16 18:55:55 +0900

    add create action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,28 @@
 class PagesController < ApplicationController
-  def index
+  def new 
+    @page = Page.new
   end
+
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      flash[:success] = "Successfuly page created"
+      redirect_to @page
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  private
+    def page_params
+      params.require(:page).permit(:title, :content)
+    end
 end
