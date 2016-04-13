27ba13ce2bd0a9850ba7b57c53886a111db4ed71
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2016-04-14 00:37:44 +0900

    update pages.js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,17 +1,116 @@
 (function() {
-  console.log("Hello Backbone.js");
-
-  var Model = Backbone.Model.extend({
-    defaults: {
-      "title": '',
+  var Page = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        title    : "title",
+        //tag      : "",
+        content  : ""
+      };
     },
-    initialize: function() {
-      console.log("Model");
+
+    validate: function (attributes) {
+      if(attributes.title === "") {
+        return "title must not be empty.";
+      }
     }
   });
 
-  var model = new Model();
-  model.set({title: "first"});
+  var Pages = Backbone.Collection.extend({
+    model : Page,
+    url   : "/pages.json",
 
-  console.log(model);
+    // parse: function (res) {
+    //   if(res.error) {
+    //     console.log("PagesCollection: "+ res.error.message);
+    //   }
+    //   return res.list;
+    // }
+  });
+
+  var PageView = Backbone.View.extend({
+    tagName: 'ul',
+
+    template: _.template($('#pages-template').html()),
+
+    render: function () {
+      this.$el.html(this.template(this.model.attributes));
+      return this;
+    },
+  })
+
+  var PagesView = Backbone.View.extend({
+    el: $('#main'),
+
+    initialize: function () {
+      _.bindAll(this, "render", "appendItem", "resetItems");
+      this.listenTo(this.collection, 'add', this.appendItem);
+      this.listenTo(this.collection, 'reset', this.resetItems);
+      this.listenTo(this.collection, 'sync', this.render);
+      this.collection.fetch();
+    },
+
+    render: function () {
+      _(this.collection).each(function (model) {
+        this.collection.add(model);
+      }, this);
+    },
+
+    appendItem: function(model) {
+      var view = new PageView({ model: model });
+      this.$el.html(view.render().el);
+    },
+
+    resetItems: function (collection) {
+      collection.each(function(model) {
+        this.appendItem(model);
+      }, this);
+    },
+
+    error: function () {
+      console.log("error: PagesView");
+    },
+  });
+
+  var HeaderView = Backbone.View.extend({
+    events: {
+      "click #create" : "onCreate",
+      "click #edit"   : "onEdit"
+    },
+    onCreate: function () {
+      router.navigate("create", { trigger: true });
+    },
+    onEdit: function () {
+      router.navigate("edit", { trigger: true });
+    },
+
+  });
+
+  var Router = Backbone.Router.extend({
+    routes: {
+      ''         : 'index',
+      'page/:id' : 'show',
+      'edit/:id' : 'edit',
+      'create'   : 'create',
+    },
+    initialize: function () {
+      this.headerview = new HeaderView({ el: $('#header') });
+      this.pages = new Pages();
+    },
+
+    index: function() {
+      var view = new PagesView({collection: this.pages});
+    },
+
+    show: function (id) {
+    },
+
+    edit: function (id) {
+    },
+
+    create: function () {
+    }
+  });
+
+  var router = new Router();
+  Backbone.history.start();
 })();
